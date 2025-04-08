419e80e146d559887bddcb1f25ad9c68a7c691e1
Thanh <thanhdev25@gmail.com>
2025-04-08 13:20:01 +0700

    Initial project

---
+++ src/main/java/net/thanhdevjava/personal_blogging_platform/PersonalBloggingPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package net.thanhdevjava.personal_blogging_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBloggingPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBloggingPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/thanhdevjava/personal_blogging_platform/PersonalBloggingPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.thanhdevjava.personal_blogging_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBloggingPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
